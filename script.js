5ebf28c6cd5de2334a70fb40abc9372b9023da76
swethamadhan <79351032+swethamadhan@users.noreply.github.com>
2021-05-05 17:07:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,5 @@ function compute()
 }
  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
    
+
 }
